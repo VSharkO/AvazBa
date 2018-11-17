4d3b939c6bff99f771d3f0a088e89a601e172e5a
Valentin Šarić <valentin.saric1@gmail.com>
2018-11-18 00:17:16 +0100

    created costume cell

---
+++ AvazBa/MainScreen/Custom cells/CustomCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomCell.swift
+//  AvazBa
+//
+//  Created by Valentin Šarić on 18/11/2018.
+//  Copyright © 2018 Valentin Šarić. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
