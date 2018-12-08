b549050802810e1427f9087fbed5a0a7cddece41
VSharkO <valentin.saric1@gmail.com>
2018-12-08 20:45:30 +0100

    titleCell addd

---
+++ AvazBa/SingleScreen/Cells/TitleCell.swift
@@ -0,0 +1,15 @@
+//
+//  TitleCell.swift
+//  AvazBa
+//
+//  Created by Valentin Šarić on 08/12/2018.
+//  Copyright © 2018 Valentin Šarić. All rights reserved.
+//
+
+import UIKit
+
+class TitleCell: UITableViewCell {
+    
+    let
+
+}
